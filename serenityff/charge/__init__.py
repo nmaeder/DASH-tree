08430928817ef758cedf19ac8491c6073d485721
Niels Maeder <maedern@student.ethz.ch>
2025-12-15 14:45:59 +0100

    remove commented lines

--- serenityff/charge/__init__.py
+++ serenityff/charge/__init__.py
@@ -1,14 +1,3 @@
 # Copyright (C) 2022-2025 ETH Zurich, Niels Maeder and other DASH contributors.
 
-# from serenityff.charge.gnn import ChargeCorrectedNodeWiseAttentiveFP, NodeWiseAttentiveFP, Extractor, Trainer
-
-# from . import _version
-
 __version__ = "2.0.1"
-
-# __all__ = [
-#    Trainer,
-#    Extractor,
-#    ChargeCorrectedNodeWiseAttentiveFP,
-#    NodeWiseAttentiveFP,
-# ]
